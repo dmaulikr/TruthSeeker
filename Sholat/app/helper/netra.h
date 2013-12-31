40b434baffe532fe8d72de8c0916f1e25aa7e879
Arie <arie@wego.com>
2013-12-31 17:58:26 +0700

    add helper

---
+++ Sholat/app/helper/netra.h
@@ -0,0 +1,13 @@
+//
+//  netra.h
+//  Sholat
+//
+//  Created by Flash on 12/31/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface netra : NSObject
+
+@end
---
+++ Sholat/app/helper/netra.m
@@ -0,0 +1,13 @@
+//
+//  netra.m
+//  Sholat
+//
+//  Created by Flash on 12/31/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import "netra.h"
+
+@implementation netra
+
+@end
